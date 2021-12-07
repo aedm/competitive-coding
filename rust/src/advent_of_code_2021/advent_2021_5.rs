e773293a70ebbc29ced6e35630c400be013585b2
aedm <korteur@gmail.com>
2021-12-07 11:28:56 +0100

    Tweak

--- rust/src/advent_of_code_2021/advent_2021_5.rs
+++ rust/src/advent_of_code_2021/advent_2021_5.rs
@@ -5,7 +5,7 @@ use std::cmp::{max, min};
 fn order_swap(x: i64, y: i64) -> (i64, i64) {
     (min(x, y), max(x, y))
 }
-ł
+
 pub fn solve(diagonals: bool) -> i64 {
     let lines = read_lines("advent_2021/5.txt");
     let rows = lines
@@ -17,12 +17,12 @@ pub fn solve(diagonals: bool) -> i64 {
         if r[0] == r[2] || r[1] == r[3] {
             let (x1, x2) = order_swap(r[0], r[2]);
             let (y1, y2) = order_swap(r[1], r[3]);
-            ((y1..=y2).cartesian_product(x1..=x2)).for_each(|c| fields.push(c));
+            ((x1..=x2).cartesian_product(y1..=y2)).for_each(|c| fields.push(c));
         } else if diagonals {
             let (a, b) = order_swap(r[0], r[2]);
             let dx = (r[2] > r[0]) as i64 * 2 - 1;
             let dy = (r[3] > r[1]) as i64 * 2 - 1;
-            (0..=(b - a)).for_each(|n| fields.push((r[1] + n * dy, r[0] + n * dx)));
+            (0..=(b - a)).for_each(|n| fields.push((r[0] + n * dx, r[1] + n * dy)));
         }
     }
     fields.sort();
