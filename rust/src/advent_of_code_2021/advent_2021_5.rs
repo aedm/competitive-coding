79623a576362a7a5b3887c38feeb2620b3533a52
aedm <korteur@gmail.com>
2021-12-07 13:24:53 +0100

    Tweak

--- rust/src/advent_of_code_2021/advent_2021_5.rs
+++ rust/src/advent_of_code_2021/advent_2021_5.rs
@@ -8,10 +8,12 @@ pub fn solve(diagonals: bool) -> i64 {
         .map(|x| x.replace(" -> ", ",").split(',').map(|s| s.parse::<i64>().unwrap()).collect_vec())
         .collect_vec();
     let mut fields = vec![];
-    for r in rows.iter().filter(|r| r[0] == r[2] || r[1] == r[3] || diagonals) {
-        let (dx, dy) = ((r[2] - r[0]).signum(), (r[3] - r[1]).signum());
-        let length = max((r[2] - r[0]).abs(), (r[3] - r[1]).abs());
-        (0..=length).for_each(|n| fields.push((r[0] + n * dx, r[1] + n * dy)));
+    for r in rows {
+        if r[0] == r[2] || r[1] == r[3] || diagonals {
+            let (rx, ry) = (r[2] - r[0], r[3] - r[1]);
+            let (dx, dy) = (rx.signum(), ry.signum());
+            (0..=max(rx.abs(), ry.abs())).for_each(|n| fields.push((r[0] + n * dx, r[1] + n * dy)));
+        }
     }
     fields.sort();
     fields.iter().group_by(|&&e| e).into_iter().map(|x| (x.1.count() >= 2) as i64).sum()
