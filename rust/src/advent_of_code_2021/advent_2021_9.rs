ec4e39d4ea4d1642ea822fdba0779b0aae0e1413
aedm <korteur@gmail.com>
2021-12-09 11:27:37 +0100

    AoC 2021/9 tweak

--- rust/src/advent_of_code_2021/advent_2021_9.rs
+++ rust/src/advent_of_code_2021/advent_2021_9.rs
@@ -7,12 +7,12 @@ use std::iter::FromIterator;
 pub fn solve_1() -> i64 {
     let lines = read_lines("advent_2021/9.txt");
     let n = lines.iter().map(|s| s.bytes().map(|b| (b - b'0') as i64).collect_vec()).collect_vec();
-    let dir = [(1i64, 0i64), (-1, 0), (0, 1), (0, -1)];
+    let dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
     let xlen = n[0].len() as i64;
     let ylen = n.len() as i64;
     let mins = ((0..xlen).cartesian_product(0..ylen))
         .filter(|&(x, y)| {
-            dir.iter().all(|&(dx, dy)| {
+            dirs.iter().all(|&(dx, dy)| {
                 (x + dx) != (x + dx).clamp(0, xlen - 1)
                     || (y + dy) != (y + dy).clamp(0, ylen - 1)
                     || n[(y + dy) as usize][(x + dx) as usize] > n[y as usize][x as usize]
@@ -25,43 +25,24 @@ pub fn solve_1() -> i64 {
 pub fn solve_2() -> usize {
     let lines = read_lines("advent_2021/9.txt");
     let n = lines.iter().map(|s| s.bytes().map(|b| (b - b'0') as i64).collect_vec()).collect_vec();
-    let dir = [(1i64, 0i64), (-1, 0), (0, 1), (0, -1)];
-    let xlen = n[0].len() as i64;
-    let ylen = n.len() as i64;
+    let dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    let mut dry_fields: BTreeSet<_> = ((0..n[0].len() as i64).cartesian_product(0..n.len() as i64))
+        .filter(|&(x, y)| n[y as usize][x as usize] < 9)
+        .collect();
 
     let mut basins = vec![];
-
-    let mut vis = BTreeSet::new();
-    for (x, y) in (0..xlen).cartesian_product(0..ylen) {
-        if n[y as usize][x as usize] == 9 {
-            vis.insert((x, y));
-        }
-    }
-
-    while (vis.len() as i64) < xlen * ylen {
-        let mut unalloc = vis.len();
-        let mut q = VecDeque::new();
-        for (x, y) in (0..xlen).cartesian_product(0..ylen) {
-            if !vis.contains(&(x, y)) {
-                vis.insert((x, y));
-                q.push_back((x, y));
-                break;
+    while let Some(e) = dry_fields.iter().cloned().next() {
+        let mut flood_count = dry_fields.len();
+        let mut queue = VecDeque::new();
+        queue.push_back(e);
+        dry_fields.remove(&e);
+        while let Some(c) = queue.pop_front() {
+            for &(dx, dy) in &dirs {
+                let nc = (c.0 + dx, c.1 + dy);
+                dry_fields.remove(&nc).then(|| queue.push_back(nc));
             }
         }
-
-        while let Some(c) = q.pop_front() {
-            for &(dx, dy) in &dir {
-                let (nx, ny) = (c.0 + dx, c.1 + dy);
-                if nx == nx.clamp(0, xlen - 1)
-                    && ny == ny.clamp(0, ylen - 1)
-                    && !vis.contains(&(nx, ny))
-                {
-                    vis.insert((nx, ny));
-                    q.push_back((nx, ny));
-                }
-            }
-        }
-        basins.push(vis.len() - unalloc);
+        basins.push(flood_count - dry_fields.len());
     }
     basins.iter().sorted().rev().take(3).product::<usize>()
 }
