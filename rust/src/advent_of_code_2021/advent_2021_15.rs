5f99e3b3eb086398a7ba317a9f8b60717d60bb02
aedm <korteur@gmail.com>
2021-12-15 11:37:31 +0100

    AoC 2021/15

--- rust/src/advent_of_code_2021/advent_2021_15.rs
+++ rust/src/advent_of_code_2021/advent_2021_15.rs
@@ -8,23 +8,22 @@ fn solve(costs: &[Vec<i64>]) -> i64 {
     let mut reach = BTreeSet::<(i64, usize, usize)>::new();
     let mut path_cost = vec![vec![i64::MAX; mx]; my];
     path_cost[0][0] = 0;
+    reach.insert((0, 0, 0));
 
-    let mut now = (0, 0);
-    let end = (mx - 1, my - 1);
-    while now != end {
-        let now_cost = path_cost[now.1][now.0];
-        reach.remove(&(now_cost, now.0, now.1));
-        for (x, y) in neighbours4(now, 0, 0, mx, my) {
-            if path_cost[y][x] > now_cost + costs[y][x] {
+    while let Some(&now) = reach.iter().next() {
+        if (now.1, now.2) == (mx - 1, my - 1) {
+            break;
+        }
+        reach.remove(&now);
+        for (x, y) in neighbours4((now.1, now.2), 0, 0, mx, my) {
+            if path_cost[y][x] > now.0 + costs[y][x] {
                 reach.remove(&(path_cost[y][x], x, y));
-                path_cost[y][x] = now_cost + costs[y][x];
+                path_cost[y][x] = now.0 + costs[y][x];
                 reach.insert((path_cost[y][x], x, y));
             }
         }
-        let next = reach.iter().next().unwrap();
-        now = (next.1, next.2);
     }
-    path_cost[end.1][end.0]
+    path_cost[my - 1][mx - 1]
 }
 
 pub fn solve_1() -> i64 {
