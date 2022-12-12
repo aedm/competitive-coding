0ad8e706524acf83733ac2c19b00c1f3857fd0c1
aedm <korteur@gmail.com>
2022-12-12 11:12:21 +0100

    AoC 2022/10

---
+++ rust/src/advent_of_code_2022/advent_2022_10.rs
@@ -0,0 +1,64 @@
+use crate::utils::read_lines;
+use itertools::Itertools;
+
+pub fn solve_1() -> i64 {
+    let mut s = 1i64;
+    let mut t = 1;
+    let mut v = vec![(s, t)];
+    for line in read_lines("advent_2022/10.txt") {
+        let p = line.split(' ').collect_vec();
+        match p[0] {
+            "addx" => {
+                s += p[1].parse::<i64>().unwrap();
+                t += 2;
+                v.push((s, t));
+            }
+            "noop" => {
+                t += 1;
+                v.push((s, t));
+            }
+            _ => panic!(),
+        }
+    }
+    let q = [20, 60, 100, 140, 180, 220];
+    q.iter()
+        .map(|x| {
+            let k = v.iter().find_position(|(s, t)| t > x).unwrap();
+            let k = v[k.0 - 1].0;
+            k * x
+        })
+        .sum()
+}
+
+pub fn solve() -> () {
+    let lines = read_lines("advent_2022/10.txt");
+    let mut it = lines.iter();
+    let mut pos = 1i64;
+    let mut nextp = 1i64;
+    let mut v = vec![String::new(); 6];
+    let mut t = 0;
+    for r in 0..6 {
+        for c in 0..40 {
+            if t == 0 {
+                pos = nextp;
+                let line = it.next().unwrap();
+                let p = line.split(' ').collect_vec();
+                match p[0] {
+                    "addx" => {
+                        nextp = pos + p[1].parse::<i64>().unwrap();
+                        t = 2;
+                    }
+                    "noop" => {
+                        nextp = pos;
+                        t = 1;
+                    }
+                    _ => panic!(),
+                }
+            }
+            t -= 1;
+            let m = (pos - c).abs() <= 1;
+            v[r].push(if m { 'X' } else { '.' });
+        }
+    }
+    println!("{:#?}", v);
+}
--- rust/src/advent_of_code_2022/mod.rs
+++ rust/src/advent_of_code_2022/mod.rs
@@ -1,4 +1,5 @@
 pub mod advent_2022_1;
+pub mod advent_2022_10;
 pub mod advent_2022_2;
 pub mod advent_2022_3;
 pub mod advent_2022_4;
