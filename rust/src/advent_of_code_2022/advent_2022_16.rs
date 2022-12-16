7172aa96ab79df31da4d806f052d4cc5835befe6
aedm <korteur@gmail.com>
2022-12-16 17:19:12 +0100

    AoC 2022/16 tweak2

--- rust/src/advent_of_code_2022/advent_2022_16.rs
+++ rust/src/advent_of_code_2022/advent_2022_16.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 use std::collections::{HashMap, HashSet};
 use std::convert::TryInto;
 
-fn read_valves() -> (Vec<(i64, Vec<usize>)>, usize) {
+fn read_valves() -> (Vec<(i32, Vec<usize>)>, usize) {
     let pattern: Regex =
         Regex::new(r"^Valve (\w+) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.*)$")
             .unwrap();
@@ -14,7 +14,7 @@ fn read_valves() -> (Vec<(i64, Vec<usize>)>, usize) {
             let caps = pattern.captures(&s).unwrap();
             (
                 caps[1].to_string(),
-                caps[2].parse::<i64>().unwrap(),
+                caps[2].parse::<i32>().unwrap(),
                 caps[3].split(", ").map(|s| s.to_string()).collect_vec(),
             )
         })
@@ -61,43 +61,42 @@ pub fn solve_1() -> i64 {
         state = new_state;
     }
 
-    *state.values().max().unwrap()
+    *state.values().max().unwrap() as i64
 }
 
 pub fn solve_2() -> i64 {
     let (valves, start) = read_valves();
-    let mut state = HashMap::from([((start, start, 0), 0)]);
-    let mut maxs = HashMap::from([((start, start, 0), 0)]);
     let non_empty_count = valves.iter().filter(|v| v.0 > 0).count();
+    let mut state = vec![(start, start, 0, 0)];
+    let len = valves.len();
+    let mut maxs = vec![-1; len * len * (1 << non_empty_count)];
     let mut m = 0;
     for time in 1..=26 {
         for _ in 0..2 {
-            let mut new_state = HashMap::new();
-            for (&(pos, other, opened), &total) in &state {
+            let mut new_state = vec![];
+            for &(pos, other, opened, total) in &state {
                 if pos < non_empty_count && (opened & (1 << pos)) == 0 {
-                    let new_key = (other, pos, opened | (1 << pos));
                     let new_total = total + valves[pos].0 * (26 - time);
-                    if maxs.get(&new_key).cloned().unwrap_or_default() < new_total {
-                        new_state.insert(new_key, new_total);
-                        maxs.insert(new_key, new_total);
+                    let new_opened = opened | (1 << pos);
+                    let mr = &mut maxs[(new_opened * len + pos) * len + other];
+                    if *mr < new_total {
+                        *mr = new_total;
                         if new_total > m {
                             m = new_total;
                         }
+                        new_state.push((other, pos, new_opened, new_total));
                     }
                 }
                 for &next_valve in &valves[pos].1 {
-                    let new_key = (other, next_valve, opened);
-                    if let Some(v) = maxs.get(&new_key) {
-                        if *v >= total {
-                            continue;
-                        }
+                    let mr = &mut maxs[(opened * len + next_valve) * len + other];
+                    if *mr < total {
+                        *mr = total;
+                        new_state.push((other, next_valve, opened, total));
                     }
-                    new_state.insert(new_key, total);
-                    maxs.insert(new_key, total);
                 }
             }
             state = new_state;
         }
     }
-    m
+    m as i64
 }
