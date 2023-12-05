f1faf09336a10017b82989e2f991f02c07721d85
aedm <korteur+fork@gmail.com>
2023-12-05 11:42:08 +0100

    AoC 2023/5

--- rust/src/advent_of_code_2023/advent_2023_5.rs
+++ rust/src/advent_of_code_2023/advent_2023_5.rs
@@ -24,22 +24,20 @@ fn solve(maps: &[Vec<(usize, usize, usize)>], seeds: &[(usize, usize)]) -> usize
     let mut seeds = seeds.to_vec();
     for map in maps {
         let mut next_seeds = vec![];
-        'l: while let Some(s) = seeds.pop() {
-            for m in map {
-                if !(m.1 >= s.1 || s.0 >= m.1 + m.2) {
-                    let is = s.0.clamp(m.1, m.1 + m.2);
-                    let ie = s.1.clamp(m.1, m.1 + m.2);
-                    next_seeds.push((is + m.0 - m.1, ie + m.0 - m.1));
-                    if s.0 < is {
-                        seeds.push((s.0, is));
-                    }
-                    if s.1 > ie {
-                        seeds.push((ie, s.1));
-                    }
-                    continue 'l;
+        while let Some(s) = seeds.pop() {
+            if let Some(m) = map.iter().find(|m| !(m.1 >= s.1 || s.0 >= m.1 + m.2)) {
+                let is = s.0.clamp(m.1, m.1 + m.2);
+                let ie = s.1.clamp(m.1, m.1 + m.2);
+                next_seeds.push((is + m.0 - m.1, ie + m.0 - m.1));
+                if s.0 < is {
+                    seeds.push((s.0, is));
                 }
+                if s.1 > ie {
+                    seeds.push((ie, s.1));
+                }
+            } else {
+                next_seeds.push((s.0, s.1));
             }
-            next_seeds.push((s.0, s.1));
         }
         seeds = next_seeds;
     }
@@ -54,6 +52,6 @@ pub fn solve_1() -> impl Debug {
 
 pub fn solve_2() -> impl Debug {
     let (seeds, maps) = read_input();
-    let mut seeds = seeds[..].chunks(2).map(|c| (c[0], c[0] + c[1])).collect_vec();
+    let seeds = seeds[..].chunks(2).map(|c| (c[0], c[0] + c[1])).collect_vec();
     solve(&maps, &seeds)
 }
