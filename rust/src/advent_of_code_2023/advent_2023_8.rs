fa696a798e30ce1a34947af2bf72ea27aed31a6d
aedm <korteur+fork@gmail.com>
2023-12-08 12:55:46 +0100

    AoC 2023/8

---
+++ rust/src/advent_of_code_2023/advent_2023_8.rs
@@ -0,0 +1,44 @@
+use crate::utils::read_lines;
+use itertools::Itertools;
+use regex::Regex;
+use std::collections::HashMap;
+use std::fmt::Debug;
+
+fn read_input() -> (Vec<char>, HashMap<String, (String, String)>) {
+    let pattern: Regex = Regex::new(r"^(...) = .(...), (...).$").unwrap();
+    let lines = read_lines("advent_2023/8.txt");
+    let dirs = lines[0].chars().collect_vec();
+    let junc = lines[2..]
+        .iter()
+        .map(|l| {
+            let s = pattern.captures(l).unwrap();
+            (s[1].to_string(), (s[2].to_string(), s[3].to_string()))
+        })
+        .collect::<HashMap<_, _>>();
+    (dirs, junc)
+}
+
+pub fn solve() -> impl Debug {
+    let (dirs, junc) = read_input();
+    let (mut s, mut i) = ("AAA", 0);
+    while s != "ZZZ" {
+        s = if dirs[i % dirs.len()] == 'R' { &junc[s].1 } else { &junc[s].0 };
+        i += 1;
+    }
+    i
+}
+
+pub fn solve_2() -> impl Debug {
+    let (dirs, junc) = read_input();
+    junc.keys()
+        .filter(|k| k.ends_with('A'))
+        .map(|mut s| {
+            let mut i = 0;
+            while !s.ends_with('Z') {
+                s = if dirs[i % dirs.len()] == 'R' { &junc[s].1 } else { &junc[s].0 };
+                i += 1;
+            }
+            i as u128
+        })
+        .fold(1, num::integer::lcm)
+}
--- rust/src/advent_of_code_2023/mod.rs
+++ rust/src/advent_of_code_2023/mod.rs
@@ -5,3 +5,4 @@ pub mod advent_2023_4;
 pub mod advent_2023_5;
 pub mod advent_2023_6;
 pub mod advent_2023_7;
+pub mod advent_2023_8;
