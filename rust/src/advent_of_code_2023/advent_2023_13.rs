4b12f9989f6eb201ca79914882f71436f0dde47f
aedm <korteur+fork@gmail.com>
2023-12-14 17:48:13 +0100

    AoC 2023/13

--- rust/src/advent_of_code_2023/advent_2023_13.rs
+++ rust/src/advent_of_code_2023/advent_2023_13.rs
@@ -2,23 +2,21 @@ use crate::utils::read_lines;
 use itertools::Itertools;
 use std::cmp::min;
 
-fn solve(smudge_count: usize) -> usize {
+fn solve(s: usize) -> usize {
     read_lines("advent_2023/13.txt")
         .split(|l| l.is_empty())
         .map(|b| {
             let b = b.iter().map(|l| l.chars().collect_vec()).collect_vec();
             let (w, h) = (b[0].len(), b.len());
             let vertical = (1..w).find(|&x| {
-                Itertools::cartesian_product(0..min(x, w - x), 0..h)
+                s == Itertools::cartesian_product(0..min(x, w - x), 0..h)
                     .filter(|&(i, j)| b[j][x - i - 1] != b[j][x + i])
                     .count()
-                    == smudge_count
             });
             let horizontal = (1..h).find(|&y| {
-                Itertools::cartesian_product(0..min(y, h - y), 0..w)
+                s == Itertools::cartesian_product(0..min(y, h - y), 0..w)
                     .filter(|&(i, j)| b[y - i - 1][j] != b[y + i][j])
                     .count()
-                    == smudge_count
             });
             horizontal.unwrap_or(0) * 100 + vertical.unwrap_or(0)
         })
