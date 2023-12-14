fffb7d8bb5aa5404926336847527a076a1271f5c
aedm <korteur+fork@gmail.com>
2023-12-14 17:25:25 +0100

    AoC 2023/13

--- rust/src/advent_of_code_2023/advent_2023_13.rs
+++ rust/src/advent_of_code_2023/advent_2023_13.rs
@@ -2,7 +2,7 @@ use crate::utils::read_lines;
 use itertools::Itertools;
 use std::cmp::min;
 
-pub fn solve_1() -> usize {
+fn solve(smudge_count: usize) -> usize {
     read_lines("advent_2023/13.txt")
         .split(|l| l.is_empty())
         .map(|b| {
@@ -10,14 +10,20 @@ pub fn solve_1() -> usize {
             let (w, h) = (b[0].len(), b.len());
             let vertical = (1..w)
                 .find(|&x| {
-                    let lc = min(x, w - x);
-                    (0..lc).cartesian_product(0..h).all(|(i, j)| b[j][x - i - 1] == b[j][x + i])
+                    (0..min(x, w - x))
+                        .cartesian_product(0..h)
+                        .filter(|&(i, j)| b[j][x - i - 1] != b[j][x + i])
+                        .count()
+                        == smudge_count
                 })
                 .unwrap_or(0);
             let horizontal = (1..h)
                 .find(|&y| {
-                    let lc = min(y, h - y);
-                    (0..lc).cartesian_product(0..w).all(|(i, j)| b[y - i - 1][j] == b[y + i][j])
+                    (0..min(y, h - y))
+                        .cartesian_product(0..w)
+                        .filter(|&(i, j)| b[y - i - 1][j] != b[y + i][j])
+                        .count()
+                        == smudge_count
                 })
                 .unwrap_or(0);
             horizontal * 100 + vertical
@@ -25,34 +31,10 @@ pub fn solve_1() -> usize {
         .sum::<usize>()
 }
 
+pub fn solve_1() -> usize {
+    solve(0)
+}
+
 pub fn solve_2() -> usize {
-    read_lines("advent_2023/13.txt")
-        .split(|l| l.is_empty())
-        .into_iter()
-        .map(|b| {
-            let b = b.iter().map(|l| l.chars().collect_vec()).collect_vec();
-            let (w, h) = (b[0].len(), b.len());
-            let vertical = (1..w)
-                .find(|&x| {
-                    let lc = min(x, w - x);
-                    (0..lc)
-                        .cartesian_product(0..h)
-                        .filter(|&(i, j)| b[j][x - i - 1] != b[j][x + i])
-                        .count()
-                        == 1
-                })
-                .unwrap_or(0);
-            let horizontal = (1..h)
-                .find(|&y| {
-                    let lc = min(y, h - y);
-                    (0..lc)
-                        .cartesian_product(0..w)
-                        .filter(|&(i, j)| b[y - i - 1][j] != b[y + i][j])
-                        .count()
-                        == 1
-                })
-                .unwrap_or(0);
-            horizontal * 100 + vertical
-        })
-        .sum::<usize>()
+    solve(1)
 }
