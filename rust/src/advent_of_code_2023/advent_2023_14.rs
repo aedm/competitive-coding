ab6d939eed91f4143c41a3e8c9fed1910d6a4514
aedm <korteur+fork@gmail.com>
2023-12-15 11:47:30 +0100

    AoC 2023/14

--- rust/src/advent_of_code_2023/advent_2023_14.rs
+++ rust/src/advent_of_code_2023/advent_2023_14.rs
@@ -3,43 +3,36 @@ use itertools::Itertools;
 use std::collections::HashMap;
 
 pub fn solve(cycles: usize) -> i64 {
-    let mut m =
+    let mut m: Vec<Vec<char>> =
         read_lines("advent_2023/14.txt").iter().map(|l| l.chars().collect_vec()).collect_vec();
     let (w, h) = (m[0].len() as i64, m.len() as i64);
 
     let dirs = [(0, -1), (-1, 0), (0, 1), (1, 0)];
     let mut map_to_index = HashMap::new();
-    let mut index_to_map = HashMap::<usize, Vec<Vec<char>>>::new();
+    let mut maps: Vec<Vec<Vec<char>>> = vec![];
 
-    let mut i = 0;
-    while i < cycles {
+    for i in 0..cycles {
         let dir = i % 4;
-        let mut rocks = Itertools::cartesian_product(0..w, 0..h)
+        let rocks = Itertools::cartesian_product(0..w, 0..h)
             .filter(|&(x, y)| m[y as usize][x as usize] == 'O')
+            .sorted_by_key(|&(x, y)| (x * -dirs[dir].0, y * -dirs[dir].1))
             .collect_vec();
-        rocks.sort_by_key(|&(x, y)| (x * -dirs[dir].0, y * -dirs[dir].1));
-        for start in rocks {
-            let mut end = start;
-            while let Some(c) = map_add(end, dirs[dir], w, h) {
+        for mut rock in rocks {
+            m[rock.1 as usize][rock.0 as usize] = '.';
+            while let Some(c) = map_add(rock, dirs[dir], w, h) {
                 if m[c.1 as usize][c.0 as usize] != '.' {
                     break;
                 }
-                end = c;
+                rock = c;
             }
-            m[start.1 as usize][start.0 as usize] = '.';
-            m[end.1 as usize][end.0 as usize] = 'O';
+            m[rock.1 as usize][rock.0 as usize] = 'O';
         }
         if let Some(&prev_i) = map_to_index.get(&(m.clone(), dir)) {
-            let skips = (cycles - i) / (i - prev_i);
-            if skips > 0 {
-                i += skips * (i - prev_i);
-                m = index_to_map.get(&prev_i).unwrap().clone();
-            }
-        } else {
-            map_to_index.insert((m.clone(), dir), i);
-            index_to_map.insert(i, m.clone());
+            m = std::mem::take(&mut maps[prev_i + (cycles - i) % (i - prev_i) - 1]);
+            break;
         }
-        i += 1;
+        map_to_index.insert((m.clone(), dir), i);
+        maps.push(m.clone());
     }
     Itertools::cartesian_product(0..w, 0..h)
         .filter(|&(x, y)| m[y as usize][x as usize] == 'O')
