11dc512d68d36785ec962a3a990f0da8a4b8b955
aedm <korteur+fork@gmail.com>
2023-12-16 21:22:03 +0100

    AoC 2023/14b

--- rust/src/advent_of_code_2023/advent_2023_14.rs
+++ rust/src/advent_of_code_2023/advent_2023_14.rs
@@ -3,38 +3,8 @@ use crate::utils::read_lines;
 use itertools::Itertools;
 use std::collections::HashMap;
 
-pub fn solve_orig(cycles: usize) -> i64 {
-    let mut m = Map2D::from_text(&read_lines("advent_2023/14.txt"));
-    let mut map_to_index = HashMap::new();
-    let mut maps = vec![];
-    for i in 0..cycles {
-        let dir = i % 4;
-        let rocks = m
-            .filter(|_, v| *v == 'O')
-            .map(|(p, _)| p)
-            .sorted_by_key(|&c| c * -DIRS4[dir])
-            .collect_vec();
-        for mut rock in rocks {
-            m[rock] = '.';
-            while let Some((c, '.')) = m.add_coord(rock, DIRS4[dir]) {
-                rock = c;
-            }
-            m[rock] = 'O';
-        }
-        let key = (m.clone(), dir);
-        if let Some(&prev_i) = map_to_index.get(&key) {
-            m = Map2D::clone(&maps[prev_i + (cycles - i) % (i - prev_i) - 1]);
-            break;
-        }
-        map_to_index.insert(key, i);
-        maps.push(m.clone());
-    }
-    m.filter(|_, v| *v == 'O').map(|(p, _)| m.h - p.y).sum()
-}
-
 pub fn solve(cycles: usize) -> i64 {
     let mut m = Map2D::from_text_u8(&read_lines("advent_2023/14.txt"));
-    let now = std::time::Instant::now();
     let mut rocks_indices = HashMap::new();
     let mut history_of_rock = vec![];
     let mut rocks = m.filter(|_, v| *v == b'O').map(|(p, _)| p).collect_vec();
@@ -52,18 +22,15 @@ pub fn solve(cycles: usize) -> i64 {
         }
         rocks = new_rocks;
         if i % 4 == 3 {
-            let cc = i/4;
-            if let Some(&prev_i) = rocks_indices.get(&rocks) {
-                rocks = Vec::clone(&history_of_rock[prev_i + (cycles / 4 - cc) % (cc - prev_i) - 1]);
+            if let Some(&pi) = rocks_indices.get(&rocks) {
+                rocks = Vec::clone(&history_of_rock[pi + (cycles - i + 3) / 4 % (i / 4 - pi) - 1]);
                 break;
             }
-            rocks_indices.insert(rocks.clone(), cc);
+            rocks_indices.insert(rocks.clone(), i / 4);
             history_of_rock.push(rocks.clone());
         }
     }
-    let r =  rocks.iter().map(|p| m.h - p.y).sum();
-    println!("Elapsed without IO: {:?}", now.elapsed());
-    r
+    rocks.iter().map(|p| m.h - p.y).sum()
 }
 
 pub fn solve_1() -> i64 {
